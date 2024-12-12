5d8e1ea4f6be7f39dfc923592bf0cbca7a50a680
Gioi.Pronesti <gioi.pronesti@gmail.com>
2024-12-12 16:15:08 +0100

    add: header component to search

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+    return (
+      <header>
+        <div className="container">
+          <div>
+           Boolflix
+          </div>
+          <input type="text" placeholder="Scrivi qualcosa" className="input-search"/>
+          <div className="button">search</div>
+        </div>
+      </header>
+    )
+  }

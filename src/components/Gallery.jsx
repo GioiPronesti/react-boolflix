ab27e406f66c4cbb1199c4aa099e788c9dc0171d
Gioi.Pronesti <gioi.pronesti@gmail.com>
2024-12-12 16:16:35 +0100

    add: Gallery movie component

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,11 @@
+export default function Gallery(){
+
+    return (
+
+        <div className="container">
+            <section>
+                <h1>Gallery Poster</h1>
+            </section>
+        </div>
+    )
+}

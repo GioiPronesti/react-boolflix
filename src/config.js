bfd9b2d2fc696eadd569f5c5b0cb4b3079a2e8e2
Gioi.Pronesti <gioi.pronesti@gmail.com>
2024-12-13 19:32:30 +0100

    add: flag assets images

--- src/config.js
+++
@@ -1,7 +0,0 @@
-const BASE_URI = "http://localhost:5173";
-
-export default BASE_URI;
-
-const API_URI = "https://api.themoviedb.org/3";
-
-export { API_URI };
---
+++ src/flags.js
@@ -0,0 +1,15 @@
+import itFlag from "./assets/flags/ita.png";
+import enFlag from "./assets/flags/en.png";
+import frFlag from "./assets/flags/fr.png";
+import deFlag from "./assets/flags/de.png";
+import jaFlag from "./assets/flags/ja.png";
+
+const flagMap = {
+  it: itFlag,
+  en: enFlag,
+  fr: frFlag,
+  de: deFlag,
+  ja: jaFlag,
+};
+
+export default flagMap;

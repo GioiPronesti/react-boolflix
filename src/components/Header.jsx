ae74ebc5d8a0d47ec8fc7ee1f7aa9b63fd1dc404
Gioi.Pronesti <gioi.pronesti@gmail.com>
2024-12-13 15:42:36 +0100

    fix: modify input added query value and set Query function on change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,26 @@
+import { useContext } from "react"
+import GlobalContext from "../contexts/GlobalContext"
+
 export default function Header() {
+
+  const { query, setQuery } = useContext(GlobalContext)
+
+  function onChange(event){
+    setQuery(event.target.value)
+  }
+
     return (
       <header>
         <div className="container">
           <div>
            Boolflix
           </div>
-          <input type="text" placeholder="Scrivi qualcosa" className="input-search"/>
-          <button type="button">Search</button>
+          <form>
+            <label htmlFor="query"></label>
+            <input type="text" name="query" id="query" value={query} onChange={onChange} placeholder="Scrivi qualcosa" />
+          </form>
+          
+          <button>Search</button>
         </div>
       </header>
     )
